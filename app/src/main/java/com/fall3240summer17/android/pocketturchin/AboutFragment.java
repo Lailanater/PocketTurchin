04513df53bf1357c63b1d522af55460d843d7633
Chris Lail <Chris Lail>
2017-06-26 18:46:45 -0400

    Initial commit

---
+++ app/src/main/java/com/fall3240summer17/android/pocketturchin/AboutFragment.java
@@ -0,0 +1,38 @@
+package com.fall3240summer17.android.pocketturchin;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends Fragment {
+
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+
+
+
+        return v;
+    }
+
+}
---
+++ app/src/main/java/com/fall3240summer17/android/pocketturchin/PocketTurchinActivity.java
@@ -0,0 +1,72 @@
+package com.fall3240summer17.android.pocketturchin;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class PocketTurchinActivity extends AppCompatActivity {
+
+    private static final int FRAGMENT_GALLERY = 0;
+    private static final int FRAGMENT_FAVS = 1;
+    private static final int FRAGMENT_SEARCH = 2;
+    private static final int FRAGMENT_ABOUT = 3;
+
+    private FragmentManager mFragmentManager;
+    private Fragment mFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_gallery:
+                    updateFragment(FRAGMENT_GALLERY);
+                    return true;
+                case R.id.navigation_favs:
+                    updateFragment(FRAGMENT_FAVS);
+                    return true;
+                case R.id.navigation_search:
+                    updateFragment(FRAGMENT_SEARCH);
+                    return true;
+                case R.id.navigation_about:
+                    updateFragment(FRAGMENT_ABOUT);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pocket_turchin);
+        updateFragment(FRAGMENT_GALLERY);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void updateFragment(int fragmentView) {
+        if (fragmentView == FRAGMENT_GALLERY) {
+            mFragment = new GalleryFragment();
+        } else if (fragmentView == FRAGMENT_FAVS) {
+            mFragment = new FavsFragment();
+        } else if (fragmentView == FRAGMENT_SEARCH) {
+            mFragment = new SearchFragment();
+        } else if (fragmentView == FRAGMENT_ABOUT){
+            mFragment = new AboutFragment();
+        }
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentManager.beginTransaction()
+                .replace(R.id.content, mFragment)
+                .commit();
+    }
+
+}
